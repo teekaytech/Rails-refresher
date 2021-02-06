a7cc87a3567dd99f3c1abba08ffb1b204e393d07
teekaytech <taofeek.olalere@feghas.com>
2021-02-06 02:34:21 +0100

    Add user registration form and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'main#index'
   get 'about-us', to: 'about#index', as: :about
+  get 'sign_up', to: 'registrations#new'
+  post 'sign_up', to: 'registrations#create'
 end
