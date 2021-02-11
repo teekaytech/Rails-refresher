37b4f7f53588ad6e37b3b97b1b8cf1323833715f
teekaytech <taofeek.olalere@feghas.com>
2021-02-11 11:25:09 +0100

    Implement sign in feature

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,18 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to root_path, notice: "Logged out!"
   end
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Logged in successfully"
+    else
+      flash[:alert] = 'Invalid email or password'
+      render :new
+    end
+  end
 end
